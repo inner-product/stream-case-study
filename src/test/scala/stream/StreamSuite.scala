7e15646d2eccdc2f1922134afb2bcecba9db9568
Noel Welsh <noel@noelwelsh.com>
2022-01-24 18:30:17 +0000

    Remove test for flatMap
    We're not implementing it at this point

--- src/test/scala/stream/StreamSuite.scala
+++ src/test/scala/stream/StreamSuite.scala
@@ -22,14 +22,4 @@ class StreamSuite extends ScalaCheckSuite {
       assertEquals(result.reverse, values.map(x => x + 1))
     }
   }
-
-  property("flatMap transforms Stream in expected way") {
-    forAll { (values: List[Int]) =>
-      val result = Stream
-        .emit(values.iterator)
-        .flatMap(x => Stream.emit(List(x, -x).iterator))
-        .foldLeft(List.empty[Int])((accum, elt) => elt :: accum)
-      assertEquals(result.reverse, values.flatMap(x => List(x, -x)))
-    }
-  }
 }
