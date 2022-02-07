933098eb4b04d74d05f8d0ecc32fe20f7ef00e25
Noel Welsh <noel@noelwelsh.com>
2022-02-07 19:53:03 +0000

    Restore to original state

--- src/main/scala/stream/Stream.scala
+++ src/main/scala/stream/Stream.scala
@@ -8,16 +8,7 @@ sealed trait Stream[A] {
   def filter(pred: A => Boolean): Stream[A] =
     ???
 
-  def interleave(that: Stream[A]): Stream[A] =
-    ???
-
   def map[B](f: A => B): Stream[B] =
-    Map(this, f)
-
-  def merge[B](that: Stream[B]): Stream[Either[A, B]] =
-    ???
-
-  def zip[B](that: Stream[B]): Stream[(A, B)] =
     ???
 
   // Interpreters
@@ -29,19 +20,13 @@ sealed trait Stream[A] {
     }
 
   def next: Option[A] =
-    this match {
-      case Emit(values)   => values.nextOption()
-      case Map(source, f) => source.next.map(f)
-    }
+    ???
 
   def toList: List[A] =
     foldLeft(List.empty[A])((lst, elt) => elt :: lst).reverse
 
 }
 object Stream {
-  final case class Map[A, B](source: Stream[A], f: A => B) extends Stream[B]
-  final case class Emit[A](values: Iterator[A]) extends Stream[A]
-
   /** Creates an infinite Stream that always produces the given value */
   def constant[A](value: A): Stream[A] = ???
   def emit[A](values: Iterator[A]): Stream[A] = Emit(values)
