f65290dd36fdbe329fe00ca8efcf91371e702478
Noel Welsh <noel@noelwelsh.com>
2022-02-07 13:57:52 +0000

    Add benchmarks and documentation

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Stream Case Study
+
+
+## Benchmarks
+
+To run them from within sbt:
+
+``` sh
+project benchmarks
+Jmh / run
+```
+
+If the benchmarks take too long (they probably will) you can try
+
+``` sh
+Jmh / run -i 3 -wi 1 -f1 -t1
+```
+
---
+++ benchmarks/src/main/scala/stream/StreamBenchmarks.scala
@@ -0,0 +1,17 @@
+package stream
+
+import org.openjdk.jmh.annotations._
+
+class StreamBenchmarks {
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  def multipleFilters(): Unit = {
+    Stream
+      .emit(Iterator.range(0, 1000000))
+      .filter(x => x > 1000)
+      .filter(x => x < 10000)
+      .foldLeft(0)(_ + _)
+    ()
+  }
+
+}
